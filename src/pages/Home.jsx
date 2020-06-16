3f860471f385da77736761b79e28d52d97cca675
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2020-06-16 00:54:06 -0500

    :alien: Updating code due to external API changes.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Hola soy home</h1>
+            <h1>Hola soy home y esto es un una imagen</h1>
+            <img className='img-fluid' src='/image_umi.jpg'/>
         </div>
     )
 }
